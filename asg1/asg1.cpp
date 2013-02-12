6ba4d72f28bdfb6c505b2e9fd01f7a33626a7011
FRI <fri@hypnotoad.(none)>
2013-02-12 16:36:54 -0600

    Test version of asg1

---
+++ asg1/asg1.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "cmvision/Blobs.h"
+
+ros::Publisher velocity_pub;
+
+void blobCallback(const cmvision::Blobs::ConstPtr& msg){
+	geometry_msgs::Twist output;
+
+
+/*****************************
+*     Insert code here
+******************************/
+
+	
+	output.linear.x = .3;
+	output.angular.x = .3;
+
+
+
+	velocity_pub.publish(output);
+}
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "follower");
+	ros::NodeHandle n;
+
+	ros::Publisher velocity_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+	ros::Subscriber sub = n.subscribe("blobs", 1000, blobCallback);   
+
+	ros::Rate loop_rate(10);
+
+	ros::spin();
+
+
+	return 0;
+}
+
+
