c18849f94a3fe20b04261eb3ff11bf90c7952b73
FRI <piyushk@cs.utexas.edu>
2013-02-12 17:41:16 -0600

    sample code comments and print statements for students

--- asg1/asg1.cpp
+++ asg1/asg1.cpp
@@ -4,33 +4,60 @@
 
 ros::Publisher velocity_pub;
 
+// This method is called whenever a blob message is received
 void blobCallback(const cmvision::Blobs::ConstPtr& msg){
-	geometry_msgs::Twist output;
 
+  // This is the output velocity that we will publish
+  geometry_msgs::Twist output;
 
-/*****************************
-*     Insert code here
-******************************/
+  // This is an example print message
+  std::cout << "got blob message, count: " << msg->blob_count << std::endl;
 
-        std::cout << "got blob message, count: " << msg->blob_count << std::endl;
+  // first, we can check if any blobs were found
+  if (msg->blob_count > 0){
 
-	if (msg->blob_count > 0){
-	
-	output.linear.x = .3;
-	output.angular.x = .3;
+    // we may want to access / look at multiple blobs in the array
+    for (int i = 0; i < msg->blob_count; i++){
 
+      // another example print with some blob info
+      std::cout << "Detected blob " << i << " with area " << msg->blobs[i].area << std::endl;
 
+      // some things to look at
+      msg->blobs[i].area;      // blob area
 
-	velocity_pub.publish(output);
-	}
+      msg->blobs[i].x;         // blob center x
+      msg->blobs[i].y;         // blob center y
+
+      msg->blobs[i].left;      // blob left x
+      msg->blobs[i].right;     // blob right x
+      msg->blobs[i].top;       // blob top x
+      msg->blobs[i].bottom;    // blob bottom x
+      
+      // it's possible you will see multiple balls, or multiple detected
+      // blobs within 1 ball... you may want to only use info from the biggest one
+
+    }
+
+    // TODO: decide what velocities to publish based on blob info
+
+    output.linear.x = 0; // TODO: fill in this with some number for fwd velocity (meters/sec)
+    output.angular.x = 0; // TODO: fill this in with some angular velocity (radians/sec)
+
+    velocity_pub.publish(output); // publish this velocity message that we filled in
+  }
+
 }
+
 
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "follower");
 	ros::NodeHandle n;
 
+	// advertise that we will publish cmd_vel messages
 	velocity_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
+	// subscribe to blob messages and call blobCallback when they are received
 	ros::Subscriber sub = n.subscribe("blobs", 1000, blobCallback);   
 
 	ros::Rate loop_rate(10);
