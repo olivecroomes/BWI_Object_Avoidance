fcfcf392cdc93eac8c8edfd37c33ec8c500a17b6
Robert Lynch <rlynch@scruffy.(none)>
2013-03-25 22:29:21 -0500

    Added wifi python

--- asg1/src/asg1.cpp
+++ asg1/src/asg1.cpp
@@ -1,3 +1,8 @@
+/*
+By Josh Eversmann, jme2347 
+   and Robert Lynch, rml953
+*/
+
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "cmvision/Blobs.h"
@@ -7,52 +12,42 @@ ros::Publisher velocity_pub;
 // This method is called whenever a blob message is received
 void blobCallback(const cmvision::Blobs::ConstPtr& msg){
 
-  // This is the output velocity that we will publish
-  geometry_msgs::Twist output;
+	// This is the output velocity that we will publish
+	geometry_msgs::Twist output;
 
-  // This is an example print message
-  std::cout << "got blob message, count: " << msg->blob_count << std::endl;
+	// check if any blobs were found
+	if (msg->blob_count > 0){
 
-  // first, we can check if any blobs were found
-  // blobs are regions of a single color found in the camera image
-  if (msg->blob_count > 0){
+		double largeArea = 400, centerX = 320;
 
-    // we may want to access / look at multiple blobs in the array
-    // this array is all the blobs found in the latest camera image
-    // you may have multiple blobs because there were multiple balls
-    // but also because the ball may have been broken into multiple blobs
-    for (int i = 0; i < msg->blob_count; i++){
+		for (int i = 0; i < msg->blob_count; i++){
 
-      // another example print with some blob info
-      std::cout << "Detected blob " << i << " with area " << msg->blobs[i].area << std::endl;
+			// Print blob info
+			std::cout << "Detected blob " << i << " with area " << msg->blobs[i].area << std::endl;
 
-      // some things to look at
-      msg->blobs[i].area;      // blob area
+			// Get area and center for largest blob			
+			if(msg->blobs[i].area > largeArea) {
+				largeArea = msg->blobs[i].area;      // blob area
+				centerX = msg->blobs[i].x;         // blob center x
+			}
 
-      msg->blobs[i].x;         // blob center x
-      msg->blobs[i].y;         // blob center y
+			/*		
+			msg->blobs[i].y;         // blob center y
+			msg->blobs[i].left;      // blob left x
+			msg->blobs[i].right;     // blob right x
+			msg->blobs[i].top;       // blob top x
+			msg->blobs[i].bottom;    // blob bottom x
+			*/
+		}
 
-      msg->blobs[i].left;      // blob left x
-      msg->blobs[i].right;     // blob right x
-      msg->blobs[i].top;       // blob top x
-      msg->blobs[i].bottom;    // blob bottom x
-      
-      // it's possible you will see multiple balls, or multiple detected
-      // blobs within 1 ball... you may want to only use info from the biggest one
 
-    }
+		double x = (largeArea > 400) ? (10000 - largeArea)/10000 : 0; // foward velocity
+		
+		output.linear.x = x;
+		output.angular.z = -(centerX - 320)/480; // angular velocity
 
-    // TODO: decide what velocities to publish based on blob info
-
-    // you probably want to set these to zero when you do not see any blobs
-    // you may want to work on turning first, and then fwd/backward
-
-    output.linear.x = 0; // TODO: fill in this with some number for fwd velocity (meters/sec)
-    output.angular.z = 0; // TODO: fill this in with some angular velocity (radians/sec)
-
-    velocity_pub.publish(output); // publish this velocity message that we filled in
-  }
-
+		velocity_pub.publish(output); // publish this velocity message
+	}
 }
 
 
